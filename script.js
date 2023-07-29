0da5219e7e30156cddb253db2fcebeea2e817b26
Kaleab Solomon <kaleabslmn@gmail.com>
2023-07-29 20:02:53 +0300

    add search functionality and demo data

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
+function search() {
+  var searchQuery = document.getElementById("searchInput").value;
 
+  // For demonstration purposes, let's assume we have a sample response
+  var response = [
+    { title: "Attack on Titan Season 1", summary: "Summary of Season 1" },
+    { title: "Attack on Titan Season 2", summary: "Summary of Season 2" },
+    { title: "Attack on Titan Season 3", summary: "Summary of Season 3" },
+  ];
 
+  var resultsContainer = document.getElementById("results");
+  resultsContainer.innerHTML = ""; // Clear previous results
+
+  // Display the results
+  response.forEach(function (item) {
+    var resultItem = document.createElement("div");
+    resultItem.classList.add("result-item");
+    resultItem.innerHTML =
+      "<h3>" + item.title + "</h3><p>" + item.summary + "</p>";
+    resultsContainer.appendChild(resultItem);
+  });
+
+  getData();
+}
