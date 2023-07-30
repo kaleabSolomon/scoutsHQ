5ba41f4427cfe2fd3172dd59a26140e6beb81b15
Kaleab Solomon <kaleabslmn@gmail.com>
2023-07-30 19:53:33 +0300

    apply fuzzy search to one page

--- script.js
+++ script.js
@@ -13,32 +13,43 @@ async function search(page = 1) {
   const generalResponse = await fetch(
     `https://api.attackontitanapi.com/characters?page=${page}`
   );
+  console.log("not ftec");
   const generalData = await generalResponse.json();
   console.log(generalData);
-  const results = Object.values(generalData.results);
+  const results = generalData.results;
   console.log(results);
 
   // Clear previous results
   resultsContainer.innerHTML = "";
   const resultItem = document.createElement("div");
   resultItem.classList.add("result-item");
-  let resultFound = false;
-  for (let value of results) {
-    if (searchQuery == value.name.toLowerCase()) {
-      console.log(value.name.toLowerCase());
-      resultItem.innerHTML = `<p>${value.name}</p>`;
-      resultsContainer.appendChild(resultItem);
-      resultFound = true;
-      break;
-    }
-  }
-  if (resultFound == false) {
-    if (generalData.info.next_page) {
-      search(page + 1);
-    } else {
-      resultItem.innerHTML = `<p>No characters found</p>`;
-      resultsContainer.appendChild(resultItem);
-    }
-  }
+
+  // Create a new Fuse instance with your dataset and desired options
+  const fuse = new Fuse(results, {
+    keys: ["name"], // Specify the keys to search in
+    threshold: 0.6, // Adjust the threshold value to control the fuzziness
+  });
+
+  const result = fuse.search(searchQuery);
+  console.log(result);
+
+  // let resultFound = false;
+  // for (let value of results) {
+  //   if (searchQuery == value.name.toLowerCase()) {
+  //     console.log(value.name.toLowerCase());
+  //     resultItem.innerHTML = `<p>${value.name}</p>`;
+  //     resultsContainer.appendChild(resultItem);
+  //     resultFound = true;
+  //     break;
+  //   }
+  // }
+  // if (resultFound == false) {
+  //   if (generalData.info.next_page) {
+  //     search(page + 1);
+  //   } else {
+  //     resultItem.innerHTML = `<p>No characters found</p>`;
+  //     resultsContainer.appendChild(resultItem);
+  //   }
+  // }
   // // Display the results
 }
